2b32ca6bdee3b00e28b32459fac0bf2112e67046
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-02-14 03:04:31 +0100

    Add home view

--- raspberrypi/views.py
+++ raspberrypi/views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template
-from flask import request, jsonify, redirect, url_for, current_app
+from flask import request, jsonify, redirect, url_for
 from db_manager import db_manager
+from datetime import datetime
 
 views = Blueprint(__name__, 'views')
 
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route('/api/temperatures', methods=['GET'])
+def get_daily_temperatures():
+    current_date = datetime.now().date()
+    query = "SELECT * FROM temperatures WHERE timestamp = %s"
+    params = (current_date,)  # replace with your desired date
+    data = db_manager.select(query=query, params=params)
+    return jsonify(data)
