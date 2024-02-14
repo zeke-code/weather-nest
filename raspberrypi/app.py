17c23b7f5ff678c96a7cd7bf1c0e5168320356ad
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-02-14 04:30:56 +0100

    Add on_message function on web server

--- raspberrypi/app.py
+++ raspberrypi/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import json
 import paho.mqtt.client as mqtt
 from views import views
 from db_manager import db_manager
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,8 +15,16 @@ def on_connect(client, userdata, flags, rc):
     print('Connected to broker with state code: ' + str(rc))
     client.subscribe(MQTT_TOPIC)
 
-def on_message(client, userdata, msg):
-    print()
+def on_message(client, userdata, message):
+    msg_payload = message.payload.decode('utf-8')
+    msg = json.loads(msg_payload)
+    print(f'Received message: {msg}')
+
+    timestamp = datetime.fromtimestamp(msg['timestamp'])
+    formatted_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+    elements = [(msg['temperature'], msg['humidity'], formatted_timestamp)]
+    database_manager.insert(elements, 'measurements', 'temperature, humidity, timestamp')
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, 'subscriber')
 client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
@@ -22,12 +32,15 @@ client.subscribe('temperature')
 client.on_connect = on_connect
 client.on_message = on_message
 client.loop_start()
-
-app.register_blueprint(views, url_prefix='/')
-app.config['MYSQL_HOST'] = 'raspberrypi'
 app.config['MYSQL_USER'] = 'pythonUser'
 app.config['MYSQL_PASSWORD'] = 'pythonPWD'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DB'] = 'weather'
+database_manager = db_manager(app)
+
+
+app.register_blueprint(views, url_prefix='/')
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- raspberrypi/db_manager.py
+++ raspberrypi/db_manager.py
@@ -1,31 +1,46 @@
-import mysql.connector
-from flask import jsonify, current_app
+from flask import Flask
+import json
+import paho.mqtt.client as mqtt
+from views import views
+from db_manager import db_manager
+from datetime import datetime
 
-class db_manager:
-    def __init__(self, user, pwd, url, db):
-        self.connection = mysql.connector.connect(
-            user=current_app.config['MYSQL_USER'],
-            pwd=current_app.config['MYSQL_PASSWORD'],
-            url=current_app.config['MYSQL_HOST'],
-            db=current_app.config['MYSQL_DB'],
-        )
+app = Flask(__name__)
 
-    def select(self, query, params):
-        cursor = self.connection.cursor()
-        cursor.execute(query, params)
-        row_headers=[x[0] for x in cursor.description]
-        json_data=[]
-        for result in cursor.fetchall():
-            json_data.append(dict(zip(row_headers, result)))
-        cursor.close()
-        return jsonify(json_data)
-    
-    def insert(self, elements, table, cols):
-        cursor = self.connection.cursor()
-        placeholders = ', '.join(['%s'] * len(elements[0]))
-        query = 'INSERT INTO {} ({}) VALUES ({})'.format(table, cols, placeholders)
-        for element in elements:
-            cursor.execute(query, element)
-        self.connection.commit()
-        cursor.close()
-        return 'Insert was successful!', 200
+MQTT_BROKER_HOST = 'localhost'
+MQTT_BROKER_PORT = 1883
+MQTT_TOPIC = 'temperature'
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected to broker with state code: ' + str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, message):
+    msg_payload = message.payload.decode('utf-8')
+    msg = json.loads(msg_payload)
+    print(f'Received message: {msg}')
+
+    timestamp = datetime.fromtimestamp(msg['timestamp'])
+    formatted_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+    elements = [(msg['temperature'], msg['humidity'], formatted_timestamp)]
+    database_manager.insert(elements, 'measurements', 'temperature, humidity, timestamp')
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, 'subscriber')
+client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
+client.subscribe('temperature')
+client.on_connect = on_connect
+client.on_message = on_message
+client.loop_start()
+app.config['MYSQL_USER'] = 'pythonUser'
+app.config['MYSQL_PASSWORD'] = 'pythonPWD'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'weather'
+database_manager = db_manager(app)
+
+
+app.register_blueprint(views, url_prefix='/')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
