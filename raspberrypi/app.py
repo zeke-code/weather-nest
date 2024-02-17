5deae3354d2282f7b9ef493b3d18e24f7efec1e6
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-02-17 17:29:37 +0100

    Remove old python Flask files

--- raspberrypi/app.py
+++
@@ -1,8 +0,0 @@
-from app_factory import create_app
-from mqtt_client import init_mqtt_client
-
-app = create_app()
-
-if __name__ == '__main__':
-    init_mqtt_client(app)
-    app.run(port=8000, debug=True)
